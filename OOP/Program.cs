596698940d64b7f70babff982d422ca55953f1d6
fox <textfox10000@gmail.com>
2025-07-24 00:07:56 +0300

    Project 01

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,9 +1,72 @@
-﻿namespace OOP
+﻿using OOP.Classes;
+
+namespace OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region First Project
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+
+
+            //Point3D P1 = new Point3D();
+            //Console.WriteLine("Please Enter First Point Cordinates: ");
+            //Console.Write("X: ");
+            //int.TryParse(Console.ReadLine(), out int x1);
+            //P1.X = x1;
+            //Console.Write("Y: ");
+            //int y1 = int.Parse(Console.ReadLine());
+            //P1.Y = y1;
+            //Console.Write("Z: ");
+            //int z1 = Convert.ToInt32(Console.ReadLine());
+            //P1.Z = z1;
+            //Console.Clear();
+
+            //Point3D P2 = new Point3D();
+            //Console.WriteLine("Please Enter First Point Cordinates: ");
+            //Console.Write("X: ");
+            //int.TryParse(Console.ReadLine(), out int x2);
+            //P2.X = x2;
+            //Console.Write("Y: ");
+            //int y2 = int.Parse(Console.ReadLine());
+            //P2.Y = y2;
+            //Console.Write("Z: ");
+            //int z2 = Convert.ToInt32(Console.ReadLine());
+            //P2.Z = z2;
+            //Console.Clear();
+            //Console.WriteLine(P1);
+            //Console.WriteLine(P2);
+
+            //Point3D P1 = new Point3D(10, 10, 10);
+            //Point3D P2 = new Point3D(10, 10, 10);
+
+
+            //if (P1 == P2) Console.WriteLine("P1 = P2"); // U Need To Override Operators
+            //else Console.WriteLine("P1 not Equal P2");
+
+            //Point3D[] point3Ds = new Point3D[]
+            //{
+            //    new Point3D(3, 1, 0),
+            //    new Point3D(1, 5, 2),
+            //    new Point3D(2, 2, 3),
+            //    new Point3D(1, 2, 4),
+            //    new Point3D(3, 2, 6)
+            //};
+
+            //Array.Sort(point3Ds);
+            //foreach (Point3D p in point3Ds)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+
+            //Point3D Cloned = (Point3D)P1.Clone();
+            #endregion
+
+
+
         }
     }
 }
